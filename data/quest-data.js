287283d88aed05abb1d03a34888e8e803db08c43
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-25 14:53:41 -0700

    adds boilerplate HTML for map and quest pages

---
+++ data/quest-data.js
@@ -0,0 +1,12 @@
+const pirates = {
+    id: 'pirates',
+    title: 'A band of Pirates',
+    map: {
+        top: '70%',
+        left: '30%'
+    },
+    image: '../assets/pirates.jpeg',
+    description: `
+    A band of space pirates boards your ship and demands 10,000 monies, and they mean business. What do you do?
+    `
+}
